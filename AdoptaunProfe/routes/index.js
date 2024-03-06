c1801b24c175991074e4ef9f393d77f74e2f89a8
DiegoMontero747 <100955153+DiegoMontero747@users.noreply.github.com>
2024-03-06 12:28:54 +0100

    correccion jaimito

--- AdoptaunProfe/routes/index.js
+++ AdoptaunProfe/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Jaimito' });
+  res.render('index', { title: 'AdoptaUnProfe' });
 });
 
 module.exports = router;
