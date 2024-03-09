e7eedad346dc421881e1fdaf6e13bf8c53f9844f
DiegoMontero747 <diemon01@ucm.es>
2024-03-09 11:21:55 +0100

    correccion de usuario de la base de datos

--- AdoptaunProfe/database.js
+++ AdoptaunProfe/database.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 
 var datosBD = {
     host:"database-1.cbu262uig8d7.eu-north-1.rds.amazonaws.com",
-    user:"admin",
+    user:"AUP",
     password:"Abrete01",
     database:"adoptaunprofe"
   }
