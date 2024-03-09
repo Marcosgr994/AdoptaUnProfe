755960681d6ec67cf31863ac343f6a9b67355086
DiegoMontero747 <diemon01@ucm.es>
2024-03-09 11:45:45 +0100

    Correccion del centrado de la página

--- AdoptaunProfe/routes/services.js
+++ AdoptaunProfe/routes/services.js
@@ -34,4 +34,33 @@ router.get('/BuscarProfesorPorKeyword/:keyword', function(req, res, next) {
     })
 });
 
+router.get('/BuscarProfesorPorKeyword/', function(req, res, next) {
+    var select='SELECT * from profesores order by nombre'
+    //en esta funcion se guardará tanto la informacio como el logo
+    app.pool.getConnection(function (err, conexion){
+        if(err){
+            res.status(500);
+            res.json({msg: "Error al obtener la conexión en la base de datos"});
+        } else{
+            conexion.query(select, function (error, data){
+
+                if(error){
+                    //nos aseguramos que la facultad exista
+                    conexion.release();
+                    res.status(500);
+                    res.json({msg: "error al realizar la consulta a la base de datos"});
+                } else{
+                    if(data.length > 0){
+                        res.status(200);
+                        res.json({listado: data});
+                    } else {
+                        res.status(400);
+                        res.json({msg: "No existe ningún profesor con esas características"});
+                    }
+                }
+            })
+        }
+    })
+});
+
 module.exports = router;
