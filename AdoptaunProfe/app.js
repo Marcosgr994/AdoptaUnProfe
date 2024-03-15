2bf5399c7b4aaba90e41b9e75649e047b99e3fe3
sergvazq99 <sergvazq@ucm.es>
2024-03-15 13:13:53 +0100

    front aceptar solicitud

--- AdoptaunProfe/app.js
+++ AdoptaunProfe/app.js
@@ -16,6 +16,7 @@ module.exports = {
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var servicesRouter = require('./routes/services');
+var requestsRouter=require('./routes/requests');
 
 var app = express();
 
@@ -32,6 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/services', servicesRouter);
 app.use('/users', usersRouter);
+app.use('/requests',requestsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
