272472e8c04e7223b348b8f23531e37bfa3db0ca
Unai10 <unaipiris@gmail.com>
2024-03-17 22:51:03 +0100

    validación registro de usuario y css

--- AdoptaunProfe/public/javascript/signUp.js
+++ AdoptaunProfe/public/javascript/signUp.js
@@ -1,14 +1,28 @@
-function validarContrasena() {
+const e = require("express");
+
+function validarRegistro() {
+    var user = document.getElementById("username").value;
+    var email =document.getElementById("email").value;
     var password = document.getElementById("password").value;
     var confirm_password = document.getElementById("confirm_password").value;
-
-    if (password != confirm_password) {
-        document.getElementById("confirm_password_error").innerHTML = "Las contraseñas no coinciden";
-        document.getElementById("confirm_sign_up").disabled = true;
-        return false;
-    } else {
-        document.getElementById("confirm_password_error").innerHTML = "";
+    var hay_campos_vacios = user == "" || email == "" || password == "" || confirm_password ==""
+    if (!hay_campos_vacios && password == confirm_password){
         document.getElementById("confirm_sign_up").disabled = false;
+    }
+    else{
+        document.getElementById("confirm_sign_up").disabled = true;
+    }
+    if(password != "" && confirm_password != "" && password != confirm_password){
+        document.getElementById("confirm_password_error").innerHTML = "Las contraseñas no coinciden";
+            document.getElementById("password").classList.add("is-invalid");
+            document.getElementById("confirm_password").classList.add("is-invalid");
+            return false;
+    }
+    
+    else {
+        document.getElementById("confirm_password_error").innerHTML = "";
+        document.getElementById("password").classList.remove("is-invalid");
+        document.getElementById("confirm_password").classList.remove("is-invalid");
         return true;
     }
 }
