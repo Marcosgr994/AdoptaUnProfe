0e9fb02d791d3ded09e2e6c64bb133baa0ab6cb1
Unai10 <unaipiris@gmail.com>
2024-03-17 16:32:58 +0100

    registro como modal

--- AdoptaunProfe/public/javascript/signUp.js
+++ AdoptaunProfe/public/javascript/signUp.js
@@ -1,21 +1,14 @@
-function validarContraseña() {
+function validarContrasena() {
     var password = document.getElementById("password").value;
     var confirm_password = document.getElementById("confirm_password").value;
 
     if (password != confirm_password) {
         document.getElementById("confirm_password_error").innerHTML = "Las contraseñas no coinciden";
+        document.getElementById("confirm_sign_up").disabled = true;
         return false;
     } else {
         document.getElementById("confirm_password_error").innerHTML = "";
+        document.getElementById("confirm_sign_up").disabled = false;
         return true;
     }
 }
-
-
-$(document).ready(function() {
-    // Cuando se haga clic en el botón "Registro" del primer modal
-    $('#openModalBtn').click(function() {
-      // Mostrar el segundo modal
-      $('#registroModal').modal('show');
-    });
-  });
