82830987f302f15ff7599fc3a9b5a84ef80ed769
DiegoMontero747 <diemon01@ucm.es>
2024-03-18 16:51:21 +0100

    correcciones front y base de datos

--- AdoptaunProfe/database.js
+++ AdoptaunProfe/database.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql');
 
 var datosBD = {
-    host:"localhost",
-    user:"AUP",
+    host:"database-1.cbu262uig8d7.eu-north-1.rds.amazonaws.com",
+    user:"admin",
     password:"Abrete01",
     database:"adoptaunprofe"
   }
--- AdoptaunProfe/public/javascript/solicitudesAlumno.js
+++ AdoptaunProfe/public/javascript/solicitudesAlumno.js
@@ -1,14 +1,14 @@
 $("#Buscar").click(function(){
     $.ajax({
-      url: '/services/BuscarProfesorPorKeyword/' + $("#buscador").val(),
+      url: '/alumno/solicitudes/' + $("#buscador").val(),
       dataType: 'json',
       success: function (data) {
           //si todo ha ido bien lo notificamos
           var html = "<div id='lista' class='row'>\n";
           data.listado.forEach(element => {
             html += "<div class='bg-white col-10 m-2'>\n";
-            html += "<h3 class= 'h3 text-dark'>Profesor: "+element.nombre+"</h3>\n";
-            html += "<h4 class= 'h4 text-secondary'>Estado: "+element.materia+"</h4>\n";
+            html += "<h3 class= 'h3 text-dark'>Profesor: "+element.nombre+" "+element.apellido+"</h3>\n";
+            html += "<h4 class= 'h4 text-secondary'>Estado: "+element.estado+"</h4>\n";
             html += "</div>\n";
           });
           html += "</div>\n";
