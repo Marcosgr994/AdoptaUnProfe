694caaf2c8406a59d18ffb1284861b0add09f022
DiegoMontero747 <diemon01@ucm.es>
2024-03-18 16:42:09 +0100

    creacion de input temporal para pruebas

--- AdoptaunProfe/database.js
+++ AdoptaunProfe/database.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql');
 
 var datosBD = {
-    host:"database-1.cbu262uig8d7.eu-north-1.rds.amazonaws.com",
-    user:"admin",
+    host:"localhost",
+    user:"AUP",
     password:"Abrete01",
     database:"adoptaunprofe"
   }
