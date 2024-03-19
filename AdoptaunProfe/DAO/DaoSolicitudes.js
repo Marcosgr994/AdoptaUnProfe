319ca66c1ed59b48612cecc39fdec56b5b0c319a
Washing-Mr8 <washing.22ml@gmail.com>
2024-03-19 22:27:47 +0100

    DAOSolicitudes implementado

---
+++ AdoptaunProfe/DAO/DaoSolicitudes.js
@@ -0,0 +1,25 @@
+const DaoAdoptaUnProfe = require("./DaoAdoptaUnProfe")
+
+class DaoSolicitudes extends DaoAdoptaUnProfe{
+
+    //ALta Solicitud
+    async altaSolucitud(alumnos){
+        //comprobar que llega
+      console.log(idProfesor, idAlumno, fecha, horaInicio, telefono, materia);  
+      const sql="INSERT INTO servicios (id_profesor, id_alumno, fecha, hora_inicio, telefono, materia) VALUES (?,?,?,?,?,?)";
+      return this.query(sql,[idProfesor, idAlumno, fecha, horaInicio, telefono, materia]);
+    }
+
+    // Obtener solicitud por id de alumno
+    async obtenerSolicitudPorId(idAlumno) {
+      const sql = 'SELECT p.nombre, p.apellidos, s.estado'
+      +' FROM profesores p'
+      +' INNER JOIN solicitudes s ON p.id = s.IdProfesor'
+      +' WHERE s.idAlumno = ?';
+      const result = await this.query(sql, [idAlumno]);
+      return result.length > 0 ? result : []; //devuelve solicitudes o un array vacio
+    }
+
+};
+
+module.exports=DaoSolicitudes;
--- AdoptaunProfe/routes/alumno.js
+++ AdoptaunProfe/routes/alumno.js
@@ -1,44 +1,36 @@
 var express = require('express');
 var router = express.Router();
 var app = require('../app.js');
+var DAOSolicitudes = require("../DAO/DaoSolicitudes.js");
+
+const DaoSolicitudes = new DAOSolicitudes();
 
 /* GET users listing. */
 router.get('/solicitudes', function(req, res, next) {
   res.render('solicitudesAlumno', { title: 'Solicitudes del alumno' });
 });
 
-router.get('/solicitudes/:usuario', function(req, res, next) {
+//obtenemos el listado de solicitudes
+router.get('/solicitudes/:usuario', async function(req, res, next) {
 
-  var select = 'SELECT p.nombre, p.apellidos, s.estado'
-  +' FROM profesores p'
-  +' INNER JOIN solicitudes s ON p.id = s.IdProfesor'
-  +' WHERE s.idAlumno = ?'
-  var n=req.params.usuario;
-  //Comprobamos la conexion
-  app.pool.getConnection(function (err, conexion){
-      if(err){
-          res.status(500);
-          res.json({msg: "Error al obtener la conexión en la base de datos"});
-      } else{
-          conexion.query(select, [n], function (error, data){
+  var n = req.params.usuario;
 
-              if(error){
-                  //nos aseguramos que la facultad exista
-                  conexion.release();
-                  res.status(500);
-                  res.json({msg: "error al realizar la consulta a la base de datos"});
-              } else{
-                  if(data.length > 0){
-                      res.status(200);
-                      res.json({listado: data});
-                  } else {
-                      res.status(400);
-                      res.json({msg: "No hay ninguna solicitud disponible"});
-                  }
-              }
-          })
-      }
-  })
+  try{
+    var listado = await DaoSolicitudes.obtenerSolicitudPorId(n);
+
+    if(listado.length > 0){
+        res.status(200);
+        res.json({listado});
+    } else {
+        res.status(400);
+        res.json({msg: "No hay ninguna solicitud disponible"});
+    }
+
+  } catch(error){
+    console.error("Error al listar las solicitudes:", error);
+    res.status(500).json({ error: "Error interno del servidor" });
+  }
+
 });
 
 module.exports = router;
