e839474647b0670fecb52dd67d8d24f86e6ce824
DiegoMontero747 <diemon01@ucm.es>
2024-03-22 14:01:12 +0100

    correccion yaml

--- AdoptaunProfe/tests/integracion/solicitarServicioaUnProfesor.test.js
+++ AdoptaunProfe/tests/integracion/solicitarServicioaUnProfesor.test.js
@@ -1,13 +1,10 @@
 const request = require('supertest');
 const app = require('../../app');
 
-// Obtener la URL de ngrok desde la variable de entorno, si está definida
-const ngrokUrl = process.env.NGROK_URL || 'http://localhost';
-
 describe('Test de integración de solicitud de servicio y listado', () => {
     test('Buscar profesor por palabra clave y hacer una solicitud de servicio', async () => {
         // Realizar la solicitud para buscar un profesor por palabra clave
-        const searchResponse = await request(`${ngrokUrl}/services/BuscarProfesorPorKeyword/matematicas`);
+        const searchResponse = await request(app.app).get('/services/BuscarProfesorPorKeyword/matematicas');
         expect(searchResponse.status).toBe(200);
         expect(searchResponse.body).toHaveProperty('listado');
         expect(searchResponse.body.listado).toBeInstanceOf(Array);
@@ -15,8 +12,8 @@ describe('Test de integración de solicitud de servicio y listado', () => {
         // Tomar el primer profesor de la lista como ejemplo
         const profesor = searchResponse.body.listado[0];
 
-        const solicitudResponse = await request(`${ngrokUrl}/services/SolicitarServicioAUnProfesor`)
-            .post('/')
+        const solicitudResponse = await request(app.app)
+            .post('/services/SolicitarServicioAUnProfesor')
             .send({
                 fecha: '22/02/2022',
                 horaInicio: 10,
@@ -25,7 +22,7 @@ describe('Test de integración de solicitud de servicio y listado', () => {
                 mailProfesor: profesor.email,
                 mailAlumno: 'demo1@gmail.com'
             });
-        
+        console.log(solicitudResponse.error);
         expect(solicitudResponse.status).toBe(200);
         expect(solicitudResponse.body).toHaveProperty('msg', 'Solicitud almacenada correctamente');
     });
