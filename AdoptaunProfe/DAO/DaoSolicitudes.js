7239130ca7a47dd1e62aabe0a337b8af08e95f06
danielfdez17 <daniel17dev@gmail.com>
2024-03-22 16:22:09 +0100

    backend aceptar solicitud de un alumno parece que esta arreglado

--- AdoptaunProfe/DAO/DaoSolicitudes.js
+++ AdoptaunProfe/DAO/DaoSolicitudes.js
@@ -6,6 +6,11 @@ class DaoSolicitud extends DaoAdoptaUnProfe {
     const sql = 'INSERT INTO solicitudes(emailProfesor, emailAlumno, fecha) VALUES (?, ?, ?)'
     return this.query(sql, [solicitud.emailProfesor, solicitud.emailAlumno, solicitud.fecha])
   }
+
+  async actualizarSolicitud (solicitud) {
+    const sql = 'UPDATE solicitudes SET estado = ? WHERE idProfesor = ? AND idAlumno = ? AND fecha = ? AND horaInicio = ? AND materia = ? AND telefono = ?'
+    return this.query(sql, ['ACEPTADA', solicitud.idProfesor, solicitud.idAlumno, solicitud.fecha, solicitud.horaInicio, solicitud.materia, solicitud.telefono])
+  }
 }
 
 module.exports = {
--- AdoptaunProfe/routes/solicidutes.js
+++ AdoptaunProfe/routes/solicidutes.js
@@ -1,28 +1,44 @@
 const express = require('express')
 const router = express.Router()
-const app = require('../app.js')
+// const app = require('../app.js')
+
+const DaoSolicitudes = require('../DAO/DaoSolicitudes.js')
+
+const daoSolicitudes = new DaoSolicitudes()
 
 router.get('/solicitudes', (req, res, next) => {
   res.render('solicitudesProfesor', { title: 'Aceptar solicitud de un alumno' })
 })
 
-router.get('/solicitudes/aceptarSolicitud:solicitud', (req, res, next) => {
-  const select = 'INSERT INTO solicitudes(emailProfesor, emailAlumno, fecha) VALUES (?, ?, ?)'
-  const { solicitud } = req.params
-  app.pool.getConnection((err, conexion) => {
-    if (err) {
-      res.status(500).json({ msg: 'Error al obtener la conexion de la BBDD' })
-    } else {
-      conexion.query(select, [solicitud.emailProfesor, solicitud.emailAlumno, solicitud.fecha], (error, data) => {
-        if (error) {
-          conexion.release()
-          res.status(500).json({ msg: 'Error al insertar la solicitud en la BBDD' })
-        } else {
-          res.status(200).json({ msg: 'Solicitud guardada en la BBDD con exito' })
-        }
-      })
-    }
-  })
+router.post('/solicitudes/aceptarSolicitud:solicitud', async (req, res, next) => {
+  try {
+    const { solicitud } = req.params
+
+    solicitud.estado = 'ACEPTADA'
+
+    await daoSolicitudes.actualizarSolicitud(solicitud)
+
+    res.status(200).json({ msg: 'Solicitud aceptada' })
+
+    // app.pool.getConnection((err, conexion) => {
+    //   if (err) {
+    //     res.status(500).json({ msg: 'Error al obtener la conexion de la BBDD' })
+    //   } else {
+
+    //     conexion.query(sql, ['ACEPTADA', solicitud.idProfesor, solicitud.idAlumno, solicitud.fecha, solicitud.horaInicio, solicitud.materia, solicitud.telefono], (error, data) => {
+    //       if (error) {
+    //         conexion.release()
+    //         res.status(500).json({ msg: 'Error al insertar la solicitud en la BBDD' })
+    //       } else {
+    //         res.status(200).json({ msg: 'Solicitud guardada en la BBDD con exito' })
+    //       }
+    //     })
+    //   }
+    // })
+  } catch (err) {
+    console.error(err)
+    res.status(500).json({ msg: 'Error interno del servidor' })
+  }
 })
 
 module.exports = {
