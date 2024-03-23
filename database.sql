8ce98fd9bcbf94784736462e4b681e14168e1cd4
AylasOscar <91012819+AndreAylas@users.noreply.github.com>
2024-03-23 23:21:13 +0100

    base de datos

--- database.sql
+++ database.sql
@@ -16,6 +16,12 @@ values
 
 ALTER TABLE `profesores` ADD `apellidos` VARCHAR(30) NOT NULL AFTER `nombre`;
 
+ALTER TABLE `profesores` ADD `rangos`VARCHAR(40) not NULL AFTER `materia`;
+
+ALTER TABLE `profesores` ADD `descripcion` VARCHAR(200);
+
+ALTER TABLE `profesores`ADD `contrasena` VARCHAR(40);
+
 CREATE USER 'AUP'@'localhost' IDENTIFIED BY 'Abrete01';
 GRANT ALL PRIVILEGES ON adoptaunprofe.* TO 'AUP'@'localhost';
 FLUSH PRIVILEGES;
