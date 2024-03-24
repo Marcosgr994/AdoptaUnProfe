0f89e8dd228f3a203012f34fdc5b230676a44dd4
AylasOscar <91012819+AndreAylas@users.noreply.github.com>
2024-03-24 15:37:23 +0100

    corrigiendo codigo

---
+++ AdoptaunProfe/public/javascript/signUp.js
@@ -0,0 +1,83 @@
+// Función para validar el formulario de registro
+function validarRegistro() {
+    var user = document.getElementById("username").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var confirm_password = document.getElementById("confirm_password").value;
+    var hay_campos_vacios = user == "" || email == "" || password == "" || confirm_password == "";
+    
+    if (!hay_campos_vacios && password == confirm_password) {
+        document.getElementById("confirm_sign_up").disabled = false;
+    } else {
+        document.getElementById("confirm_sign_up").disabled = true;
+    }
+    
+    if (password != "" && confirm_password != "" && password != confirm_password) {
+        document.getElementById("confirm_password_error").innerHTML = "Las contraseñas no coinciden";
+        return false;
+    } else {
+        document.getElementById("confirm_password_error").innerHTML = "";
+        return true;
+    }
+}
+
+// Adjuntar la función validarRegistro a los eventos input en los campos del formulario
+document.getElementById('username').addEventListener('input', validarRegistro);
+document.getElementById('email').addEventListener('input', validarRegistro);
+document.getElementById('password').addEventListener('input', validarRegistro);
+document.getElementById('confirm_password').addEventListener('input', validarRegistro);
+
+
+// Función para mostrar el mensaje en el modal
+function mostrarMensajeModal(mensaje, exitoso) {
+    const mensajeModalBody = document.getElementById('mensajeModalBody');
+    const modalContent = document.querySelector('.modal-content');
+
+    // Remover clases de éxito o error previas
+    modalContent.classList.remove('modal-content-error');
+    modalContent.classList.remove('modal-content-success');
+
+    if (exitoso) {
+        modalContent.classList.add('modal-content-success');
+    } else {
+        modalContent.classList.add('modal-content-error');
+    }
+
+    // Insertar mensaje en el cuerpo del modal
+    mensajeModalBody.innerHTML = `<p class="${exitoso ? 'text-success' : 'text-danger'}">${mensaje}</p>`;
+
+    // Ocultar el modal de registro y mostrar el modal de mensaje
+    $('#registroModal').modal('hide');
+    $('#mensajeModal').modal('show');
+}
+
+// Escuchar el evento de envío del formulario
+document.getElementById('registroForm').addEventListener('submit', async function (event) {
+    event.preventDefault();
+    const formData = new FormData(this);
+    const response = await fetch(this.action, {
+        method: this.method,
+        body: formData
+    });
+    if (response.ok) {
+        // Si la respuesta es exitosa
+        const responseData = await response.json();
+        mostrarMensajeModal(responseData.message, true);
+    } else {
+        // Si hay un error en la respuesta
+        const responseData = await response.text();
+        mostrarMensajeModal(responseData, false);
+    }
+});
+
+
+// Escuchar el evento de clic en el botón "OK" del mensaje modal para cerrarlo
+document.getElementById('cerrarMensajeModal').addEventListener('click', function () {
+    $('#mensajeModal').modal('hide');
+});
+
+// Escuchar el evento de cierre del modal de registro para limpiar el mensaje
+$('#mensajeModal').on('hidden.bs.modal', function (e) {
+    const mensajeModalBody = document.getElementById('mensajeModalBody');
+    mensajeModalBody.innerHTML = '';
+});
--- AdoptaunProfe/routes/registro.js
+++ AdoptaunProfe/routes/registro.js
@@ -7,57 +7,61 @@ const { check, validationResult } = require("express-validator");
 router.use(express.json());
 const multerFactory = multer({ storage: multer.memoryStorage() });
 const db = require("../database");
-const daoProfesor =new DaoProfesor(db.pool);
+const daoProfesor = new DaoProfesor(db.pool);
 
+router.post('/Profesorsignup', multerFactory.none(), [
+    // Validación de nombre de profesor
+    check("username").isAlphanumeric().withMessage("El usuario debe contener solo letras y números"),
+    // Validación de email
+    check("email").isEmail().withMessage("El correo electrónico no es válido")
+        .custom(async (value, { req }) => {
+            const validDomains = ['.com', '.es']; // Domínios válidos
+            const domain = value.split('@')[1];
+            const domainIsValid = validDomains.some(validDomain => domain.endsWith(validDomain));
+            if (!domainIsValid) {
+                throw new Error("El dominio del correo electrónico no es válido");
+            }
 
-router.post('/Profesorsignup',multerFactory.none(),[
-  //validacion de nombre de usuario
-  check("username").isAlphanumeric().withMessage("El usuario debe contener solo letras y números"),
-  //validacion de email
-  check("email").isEmail().withMessage("El correo electrónico no es válido")
-  .custom(async (value, { req }) => {
-      const validDomains = ['.com', '.es'];
-      const domain = value.split('@')[1];
-      const domainIsValid = validDomains.some(validDomain => domain.endsWith(validDomain));
-      if (!domainIsValid) {
-          throw new Error("El dominio del correo electrónico no es válido");
-      }
+            // Verificar si existe el email
+            const profesorExistente = await daoProfesor.obtenerProfesorPorEmail(value);
+            if (profesorExistente) {
+                throw new Error("El correo electrónico ya está registrado");
+            }
 
-      // Verificar si el correo electrónico ya está registrado en la base de datos
-      const profesorExistente = await daoProfesor.obtenerProfesorPorEmail(value);
-      if (profesorExistente) {
-          throw new Error("El correo electrónico ya está registrado");
-      }
-      
-      return true;
-  }),
-   // Validación de la contraseña
-   check("password").isLength({ min: 8, max: 24 }).withMessage("La contraseña debe tener entre 8 y 24 caracteres"),
-   //validacion de repeticion de contraseña
-
-   check("confirm_password").custom((value, { req }) => {
-    if (value !== req.body.password) {
-        throw new Error("Las contraseñas no coinciden");
+            return true;
+        }),
+    // Validación de la contraseña
+    check("password").isLength({ min: 8, max: 24 }).withMessage("La contraseña debe tener entre 8 y 24 caracteres"),
+    // Validación de repetición de contraseña
+    check("confirm_password").custom((value, { req }) => {
+        if (value !== req.body.password) {
+            throw new Error("Las contraseñas no coinciden");
+        }
+        return true;
+    })
+], async (req, res) => {
+    // Comprobar errores de validación
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        // Concatenar todos los mensajes de error en una sola cadena
+        const errorMessages = errors.array().map(error => error.msg).join("\n");
+        return res.status(400).json({ error: errorMessages });
     }
-    return true;
-})], 
-async (reqmres)=>{
-  //comprobar erores de validacion
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-      // Concatenar todos los mensajes de error en una sola cadena
-      const errorMessages = errors.array().map(error => error.msg).join("\n");
-      return res.status(400).json({ error: errorMessages });
-  }
-  // //procesar el registro del alumno
-  // const {username,}
-}
-)
 
+    // Procesar el registro del profesor
+    const { username, apellidos, email, materia, rangos, desc, password } = req.body;
+    try {
+        // Realizar el alta del profesor
+        await daoProfesor.altaProfesor(username, apellidos, materia, rangos, email, desc, password);
+        res.status(200).json({ message: "Profesor registrado exitosamente" });
+    } catch (error) {
+        res.status(500).json({ error: "Error al registrar al profesor" });
+    }
+});
 
 /* GET users listing. */
-router.get('/Profesorsignup', function(req, res, next) {
+router.get('/Profesorsignup', function (req, res, next) {
     res.render('registro');
-  });
-  
-  module.exports = router;
+});
+
+module.exports = router;
